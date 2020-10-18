ccf9aa1f9071e45e3bb697bc193680101175f8c1
Teddy Leon <teddyleonweb@gmail.com>
2020-10-18 11:31:54 -0400

    Fin del testeo

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  public sum:any;
+
+  public sum1:any;
+
+
+  public resultado:number;
+  constructor() {
+
+this.sum=2;
+this.sum1=3;
+  }
+
+  ngOnInit()
+  {
+    this.sumando();
+  }
+
+ sumando(){
+
+
+
+    this.resultado= this.sum+this.sum1;
+    }
 
 }
+
+
+
+  
